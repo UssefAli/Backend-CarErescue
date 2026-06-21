3994df955ebb22e28ac9f9a9054c636f77ff44bf
UssefAli <161525322+UssefAli@users.noreply.github.com>
2026-06-21 18:19:21 +0300

    Change DATABASE_URL to DATABASE_URL_ASYNC

--- alembic/env.py
+++ alembic/env.py
@@ -28,7 +28,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL_ASYNC")
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 # Set target metadata
 target_metadata = Base.metadata
